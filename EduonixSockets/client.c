654e2fdddd41f22bd62f4197dac34533e928ed98
joseph.L <josephdlee08@gmail.com>
2023-05-30 01:34:18 +0000

    Update client.c

--- EduonixSockets/client.c
+++ EduonixSockets/client.c
@@ -5,26 +5,25 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+
 int main() {
     // creating socket
     int sock;
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    // specifying a socket address
+    // specifying an address
     struct sockaddr_in saddr;
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(5555);
-    saddr.sin_addr.s_addr = INADDR_ANY;
+    saddr.sin_addr.s_addr = INADDR_ANY; // specifies localhost, 0.0.0.0
 
-    // connecting to the server
-    int conn_status = connect(sock, (struct sockaddr *) &saddr, sizeof(saddr));
-    if(conn_status == -1) printf("There was a problem connecting to the server"); close(sock);
+    int conn = connect(sock, (struct sockaddr*) &saddr, sizeof(saddr));
+    if (conn == -1) printf("Something went wrong.");
 
-    // receiving data from the server
     char response[256];
-    recv(sock, &response, sizeof(response), 0);
-
-    printf("[SERVER] %s\n", response);
+    recv(sock, response, sizeof(response), 0);
+    
+    printf("The server sent a message: %s", response);
     close(sock);
 
     return 0;
